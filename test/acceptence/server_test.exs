c2882bc758daad8070fd661926e0411dd10d3726
Pankaj Sharma <sharmapankaj2512@gmail.com>
2019-09-27 07:43:53 +0400

    Uncomment assertion

--- test/acceptence/server_test.exs
+++ test/acceptence/server_test.exs
@@ -46,7 +46,7 @@ defmodule ServerTest do
       start_portunus do
         assert send_message(["LOCK", "myhash"]) == "+OK\r\n"
         assert send_message(["RELEASE", "myhash"]) == "+OK\r\n"
-        # assert send_message(["EXISTS", "nohash"]) == "-ERR\r\n"
+        assert send_message(["EXISTS", "nohash"]) == "-ERR\r\n"
       end
     end
   end
